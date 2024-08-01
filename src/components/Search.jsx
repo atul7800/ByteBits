9ef98a1fd443a69e16e9eb2ef1a8a2be71c2860e
atul <atulgupta.d796@gmil.com>
2024-08-02 01:51:55 +0530

    2-8-24 V2 filter by topic UI done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import banner from "../assets/images/banner.jpg";
 
@@ -26,12 +26,27 @@ function Search() {
     },
   ];
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="mt-8 flex flex-col justify-center lg:px-[150px]">
       <img className="rounded-2xl" src={banner} alt="banner img" />
       <div className="mx-[10%] mt-[-25px] flex items-center justify-between gap-2 rounded-lg bg-white p-3 text-[20px] shadow-lg md:mx-[20%]">
         <input className="outline-none" type="text" placeholder="Search" />
         <CiSearch className="text-[22px] text-gray-500" />
+      </div>
+
+      {/* filter options */}
+      <div className="mt-5 flex justify-center gap-5 sm:gap-10">
+        {tags.map((item, index) => (
+          <ul
+            onClick={() => setActiveIndex(index)}
+            className={`${index === activeIndex ? "bg-red-500 text-white" : null} ${index !== activeIndex ? "hover:scale-110 hover:border-[1px] hover:bg-white" : null} cursor-pointer rounded-lg border-red-500 p-2 px-3 transition-all duration-100 ease-in-out md:rounded-full md:px-4`}
+            key={index}
+          >
+            <li>{item.name}</li>
+          </ul>
+        ))}
       </div>
     </div>
   );
