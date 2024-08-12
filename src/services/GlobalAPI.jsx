e429651c196e173b635d03e5d26a61f6ccb9a054
atul <atulgupta.d796@gmil.com>
2024-08-12 13:04:25 +0530

    10-8-24 finished v1.0

--- src/services/GlobalAPI.jsx
+++ src/services/GlobalAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import.meta.env;
 
-const BASE_URL = "http://localhost:1337/api";
+const BASE_URL = "https://bytebits.vercel.app/";
 const BEARER_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 const api = axios.create({
