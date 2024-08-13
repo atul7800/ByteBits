ea71c974f88c5c9927d49ae9bd38e9d9463e910b
atul <atulgupta.d796@gmil.com>
2024-08-13 15:03:11 +0530

    13-8-24

--- src/services/GlobalAPI.jsx
+++ src/services/GlobalAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import.meta.env;
 
-const BASE_URL = "http://localhost:1337";
+const BASE_URL = import.meta.env.VITE_API_URL;
 const BEARER_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 const api = axios.create({
